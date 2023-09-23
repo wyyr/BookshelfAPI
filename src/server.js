1f4855ae0e618a9d6f2d4ef4632007a2edb4f13d
Waya PR <wayaragil66@gmail.com>
2023-09-23 23:30:09 +0700

    feat: add API save book

--- src/server.js
+++ src/server.js
@@ -1,10 +1,13 @@
 const Hapi = require('@hapi/hapi');
+const routes = require('./routes');
 
 const init = async () => {
   const server = Hapi.server({
     port: 9000,
     host: 'localhost',
   });
+
+  server.route(routes);
 
   console.log(`Bookshelf Application Server is running on ${server.info.uri}`);
   await server.start();
